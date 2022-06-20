934a6dacc08381f897e1566a0d915c18da776d58
Gonzalo Martinez <gon.martinez@icloud.com>
2022-06-19 23:22:38 -0300

    Prettyness changes

--- modules/proxmox/provider.tf
+++ modules/proxmox/provider.tf
@@ -8,8 +8,8 @@ terraform {
 }
 
 provider "proxmox" {
-  pm_api_url = "https://192.168.1.4:8006/api2/json"
-  pm_api_token_id = "root@pam!terraform"
-  pm_api_token_secret = "fdd0f2a1-6cf4-41a2-ba0a-c4f8ea251a6f"
+  pm_api_url = var.proxmoxUrl
+  pm_api_token_id = var.proxmoxToken
+  pm_api_token_secret = var.proxmoxSecret
   pm_tls_insecure = true
 }
