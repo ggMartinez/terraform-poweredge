4d17bfbf495d9e6c72b78925da1b50774d3af980
Gonzalo Martinez <gon.martinez@icloud.com>
2022-06-20 05:15:13 -0300

    change

--- backend.tf
+++ backend.tf
@@ -3,7 +3,7 @@ terraform {
   backend "consul" {
     address  = "192.168.1.4:8500"
     scheme   = "http"
-    path     = "tf/terraform.tfstate"
+    path     = "terraform/terraform.tfstate"
     lock     = true
     gzip     = false
   }
