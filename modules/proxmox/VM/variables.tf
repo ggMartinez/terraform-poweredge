e9c2791c93b24326497724ebd388827a9d8076ec
Gonzalo Martinez <gon.martinez@icloud.com>
2022-06-20 04:09:48 -0300

    Added pool variable

--- modules/proxmox/VM/variables.tf
+++ modules/proxmox/VM/variables.tf
@@ -11,6 +11,7 @@ variable "networkIp" {}
 variable "networkGateway" {default = "192.168.1.1"}
 variable "networkDhcp" {default = true}
 variable "dataFile" {default = ""}
+variable "pool" {default = null}
 
 
 
