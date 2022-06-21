9275cf05028212d37d6623a977f01722901c7492
Gonzalo Martinez <gonzalo.martinez@litermi.com>
2022-06-21 13:52:55 -0300

    Fix

--- modules/proxmox/VM/main.tf
+++ modules/proxmox/VM/main.tf
@@ -45,8 +45,8 @@ resource "proxmox_vm_qemu" "proxmox-vm" {
     ]
   }
   
-ipconfig0 = var.networkDhcp ? "ip=dhcp" : var.vmCount == 1 ? "ip=${var.networkIp}/24,gw=${var.networkGateway}" : "ip=${var.networkIp}${count.index + 1}/24,gw=${var.networkGateway}"  
-sshkeys = <<EOF
+  ipconfig0 = var.networkDhcp ? "ip=dhcp" : var.vmCount == 1 ? "ip=${var.networkIp}/24,gw=${var.networkGateway}" : "ip=${var.networkIp}${count.index + 1}/24,gw=${var.networkGateway}"  
+  sshkeys = <<EOF
 ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDbTYTzsMPPMuACn+CLkXTMjneeB3nHgV7s0REQ6EtR9Qta8mQpRRU6AHaM3OARr5XIRrvzy5uqMUaVrmuO/5cLEKItMf4b4q3+nL9+uXp+qO14wmBAM55vrarxhFXLiRJLwkh+7Wb8ix8O3mNKFyv1zjPp7Bv9h3H4zVdARgWuTQ5VHf9SkH9OMjA7nGVZCHYAksKlTZVTJGG1V+fT1OjygxGx14NyrTUDnQUHOR6WHyiI+FdgTLQw2utnBCflY0tmascYtbS/+sJoQHwk2j8qHmGkx+yaJZieHnjwvoO1JQQ8Gc46PIOfCRjkrn80DvLV6JiDjR7k6EiPju8V9CSeh6RJVw8SDmIwXqQwpip/00avTB6Ny3pZLFV+MdhweqFK3eDgZc4lD4U29ZO8h2XifzhPATRtfF/8uaYfE4/2TOElp/O5MO6ONsBROxw2isP5f1g4H3ifTNvTp0muC0gewhQNGodE8GzctAhf3We5Y49OMnhrxLMRF/TK3GTZS10= root@prosmos
 ${var.sshKey}
   EOF
