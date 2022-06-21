79b91ca145f89b22267c385dfcd726bde283b210
Gonzalo Martinez <gon.martinez@icloud.com>
2022-06-21 00:15:25 -0300

    test

--- resources/docker/main.tf
+++ resources/docker/main.tf
@@ -1,7 +1,7 @@
 module "proxmox-vm" {
   source = "../../modules/proxmox/VM"
   sshKey = var.sshKey
-  networkIp = "192.168.1.3"
+  networkIp = "192.168.1.4"
   name = "Dockers"
   dataFile = "${path.module}/userData.sh"
   vmCount = 3
