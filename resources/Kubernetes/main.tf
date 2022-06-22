17906e8292421cb2d5dc76af7ac7f0f2dbe8c894
Gonzalo Martinez <ggmartinez.utu@gmail.com>
2022-06-22 19:13:09 -0300

    Update main.tf
    Fix

--- resources/Kubernetes/main.tf
+++ resources/Kubernetes/main.tf
@@ -1,7 +1,7 @@
  module "kubernetes-pool"{
   source = "github.com/ggMartinez/terraform-proxmox-modules//Pool"
   id = K8s
-  comment = "Kubernetes Cluster
+  comment = "Kubernetes"
 }
 
 
@@ -10,7 +10,7 @@
   sshKey = var.sshKey
   networkDhcp = false 
   networkIp = "192.168.1.50"
-  name = "DockerTest"
+  name = "Rancher"
   pool = module.kubernetes-pool.id
   dataFile = "${path.module}/userData.sh"
 }
