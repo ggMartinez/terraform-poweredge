33fe0a89db679f85dc20dfa04a26449ff0535872
Gonzalo Martinez <gon.martinez@icloud.com>
2022-06-22 16:46:08 -0300

    K8s - Rancher

--- resources/Kubernetes/main.tf
+++ resources/Kubernetes/main.tf
@@ -1,8 +1,16 @@
+ module "kubernetes-pool"{
+  source = "github.com/ggMartinez/terraform-proxmox-modules//Pool"
+  id = K8s
+  comment = "Kubernetes Cluster
+}
+
+
  module "rancher-vm"{
   source = "github.com/ggMartinez/terraform-proxmox-modules//VM"
   sshKey = var.sshKey
   networkDhcp = false 
   networkIp = "192.168.1.50"
   name = "DockerTest"
+  pool = module.kubernetes-pool.id
   dataFile = "${path.module}/userData.sh"
 }
