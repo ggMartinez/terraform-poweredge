f9cde74e63d8cc76064dc73717e6c5585d9f4740
Gonzalo Martinez <gon.martinez@icloud.com>
2022-06-22 19:03:06 -0300

    K8s - Rancher first

--- resources/Docker/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "vm-ip" {
-   description = "VM Name and IP Address"
-   value = module.proxmox-vm.VM-Information
-}
--- resources/Kubernetes/userData.sh
+++ resources/Kubernetes/userData.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 sudo setenforce 0 
-ps aux | tr -s " " | cut -d" " -f2 | grep -q "yum"
-if [ $? -eq 0 ]
-then
-  sudo kill -9 $(ps aux | tr -s " " | cut -d" " -f2 | grep "yum")
 sudo yum install -y docker 
 sudo service docker start
 sudo chkconfig docker on
