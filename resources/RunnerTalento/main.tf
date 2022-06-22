fb15dbb7d8c5654b97dcbf861c9a12c91f5fa171
Gonzalo Martinez <gon.martinez@icloud.com>
2022-06-22 16:39:42 -0300

    Test

--- resources/RunnerTalento/main.tf
+++
@@ -1,8 +0,0 @@
- module "proxmox-vm"{
-  source = "github.com/ggMartinez/terraform-proxmox-modules//VM"
-  sshKey = var.sshKey
-  networkDhcp = false
-  networkIp = "192.168.1.199"
-  name = "GitlabRunner"
-  dataFile = "${path.module}/userData.sh"
-}
