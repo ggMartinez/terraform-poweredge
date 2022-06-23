a31a2834243e238b94483a76665a086a9059ad33
Gonzalo Martinez <gonzalo.martinez@litermi.com>
2022-06-23 03:04:52 -0300

    Go again

--- resources/Kubernetes/main.tf
+++ resources/Kubernetes/main.tf
@@ -17,31 +17,31 @@
   dataFile = "${path.module}/scripts/rancher.sh"
 }
 
-#  module "k8s-master"{
-#   source = "github.com/ggMartinez/terraform-proxmox-modules//VM"
-#   sshKey = var.sshKey
-#   networkDhcp = false 
-#   networkIp = "192.168.1.51"
-#   cpuCores = "2"
-#   Memory = "2048"
-#   name = "K8sMaster"
-#   pool = module.kubernetes-pool.id
-#   dataFile = "${path.module}/scripts/k8sMasters.sh"
-# }
+ module "k8s-master"{
+  source = "github.com/ggMartinez/terraform-proxmox-modules//VM"
+  sshKey = var.sshKey
+  networkDhcp = false 
+  networkIp = "192.168.1.51"
+  cpuCores = "2"
+  Memory = "2048"
+  name = "K8sMaster"
+  pool = module.kubernetes-pool.id
+  dataFile = "${path.module}/scripts/k8sMasters.sh"
+}
 
 
 
-#  module "k8s-workers"{
-#   source = "github.com/ggMartinez/terraform-proxmox-modules//VM"
-#   sshKey = var.sshKey
-#   networkDhcp = false 
-#   networkIp = "192.168.1.6"
-#   cpuCores = "2"
-#   vmCount = 3
-#   Memory = "2048"
-#   name = "K8sWorker"
-#   pool = module.kubernetes-pool.id
-#   dataFile = "${path.module}/scripts/k8sWorkers.sh"
-# }
+ module "k8s-workers"{
+  source = "github.com/ggMartinez/terraform-proxmox-modules//VM"
+  sshKey = var.sshKey
+  networkDhcp = false 
+  networkIp = "192.168.1.6"
+  cpuCores = "2"
+  vmCount = 3
+  Memory = "2048"
+  name = "K8sWorker"
+  pool = module.kubernetes-pool.id
+  dataFile = "${path.module}/scripts/k8sWorkers.sh"
+}
 
 
--- resources/Kubernetes/scripts/k8sMasters.sh
+++ resources/Kubernetes/scripts/k8sMasters.sh
@@ -4,6 +4,4 @@ sudo yum install -y docker
 sudo service docker start
 sudo chkconfig docker on
 
-sudo docker run -d --privileged --restart=unless-stopped --net=host -v /etc/kubernetes:/etc/kubernetes -v /var/run:/var/run  rancher/rancher-agent:v2.6.5 --server https://192.168.1.50 --token vm592z42qwpk2hpd4kmwzsfcplnznnjc75gg97dzxpd8bxwxj4694h --ca-checksum 435134d1a1231604d7f4058bfd2a2f759b9c103ef4d38211ec86e14bc31b2fa2 --etcd --controlplane         
-
-  
+sudo docker run -d --privileged --restart=unless-stopped --net=host -v /etc/kubernetes:/etc/kubernetes -v /var/run:/var/run  rancher/rancher-agent:v2.6.5 --server https://192.168.1.50 --token fs4s7nrz4vlkmghdwvhzrmmt28vpfjgmlxwgh987qbv7btqmldjjvl --ca-checksum b07b3193d383143c9674bb174e7c1f10baa405b67003bf92ac1daa1652016a9d --etcd --controlplane  
