fb67344cf4628c344957d475f96223205bb411ea
Gonzalo Martinez <gon.martinez@icloud.com>
2022-07-14 22:20:38 -0300

    Fixed user adata for k8s master worker node

--- resources/Kubernetes/scripts/k8sMasterWorker.sh
+++ resources/Kubernetes/scripts/k8sMasterWorker.sh
@@ -12,4 +12,4 @@ sudo yum install -y docker
 sudo service docker start
 sudo chkconfig docker on
 
-sudo docker run -d --privileged --restart=unless-stopped --net=host -v /etc/kubernetes:/etc/kubernetes -v /var/run:/var/run  rancher/rancher-agent:v2.6.5 --server https://192.168.1.50 --token fs4s7nrz4vlkmghdwvhzrmmt28vpfjgmlxwgh987qbv7btqmldjjvl --ca-checksum b07b3193d383143c9674bb174e7c1f10baa405b67003bf92ac1daa1652016a9d --etcd --controlplane  
+sudo docker run -d --privileged --restart=unless-stopped --net=host -v /etc/kubernetes:/etc/kubernetes -v /var/run:/var/run  rancher/rancher-agent:v2.6.6 --server https://192.168.1.50 --token 9jpflkhs99jbsgp75gbwbz2csblmlpbvhhcskdkpdcj26ds7hxkqbw --ca-checksum 7a7ce4a838183720fffe0f863e6776e7b5f97ee70eb2e0213b37f147bdd08dd4 --etcd --controlplane --worker
